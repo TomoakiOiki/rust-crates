5c4feba29919e9cef2f829f032d4acce2a59eeca
toiki <tuiboku1128@gmail.com>
2022-06-21 20:56:13 +0900

    dangling pointer周り

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
-}
+fn main() {}
+
+// 参照を返す場合ライフタイムがあっていないとエラーになる
+// fn dangle() -> &String {
+//     let s = String::from("hello");
+//     &s
+// }
+// 下記は所有権を渡すのでOK
+// fn dangle() -> String {
+//     let s = String::from("hello");
+//     s
+// }
